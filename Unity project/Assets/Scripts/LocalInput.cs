e57489f7fdb5599c2cbe2498e2cbf1e59d84d442
Edward <E.Boulderstone@newcastle.ac.uk>
2022-05-20 12:43:19 +0100

    Added comparator for rollback

--- Unity project/Assets/Scripts/LocalInput.cs
+++ Unity project/Assets/Scripts/LocalInput.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 public struct InputStruct{
     public bool jump;
     public int horizontalMove;
-    public int frameCount;
+    public override bool Equals(object obj) => this.Equals(obj is InputStruct other && this.Equals(other));
+
+    public bool Equals(InputStruct other) => jump == other.jump && horizontalMove == other.horizontalMove;
+
+    public override int GetHashCode() => (jump, horizontalMove).GetHashCode();
+    public static bool operator==(InputStruct lhs, InputStruct rhs){
+        return lhs.jump == rhs.jump && lhs.horizontalMove == rhs.horizontalMove;
+    }
+    public static bool operator!=(InputStruct lhs, InputStruct rhs){
+        return !(lhs == rhs);
+    }
 }
 public class LocalInput : MonoBehaviour
 {
